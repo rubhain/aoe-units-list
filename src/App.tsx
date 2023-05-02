6a18df0a8f99cb9021ff40c5ceec317656c35c21
rubhain <bugrahantufekcioglu@gmail.com>
2023-05-02 12:17:51 +0300

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import List from "./pages/List";
+import Home from "./pages/Home";
+import Detail from "./pages/Detail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {""}
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/units" element={<List />}></Route>
+        <Route path="/units/:id" element={<Detail />}></Route>
+      </Routes>
     </div>
   );
 }
